a531016c6092230dfe4cf427b87419fc6c0c003e
Adrien Demarez <swayndev@gmail.com>
2022-03-14 10:58:15 +0100

    models created for day 46

--- mi6-database/app/Providers/RouteServiceProvider.php
+++ mi6-database/app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    protected $namespace = 'App\\Http\\Controllers';
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
@@ -35,6 +37,13 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+
+
+            // Route::middleware('web')
+            //     ->namespace('App\Http\Controllers')
+            //     ->group(base_path('routes/admin.php'));
+
         });
     }
 
